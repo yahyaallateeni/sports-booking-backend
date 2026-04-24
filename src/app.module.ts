31044d569e0df96372cda54f116690f1106152ff
yahyaallateeni <71121853+yahyaallateeni@users.noreply.github.com>
2026-04-24 19:09:47 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
-import { HealthModule } from './modules/health/health.module';
-import { LocationsModule } from './modules/locations/locations.module';
-import { OwnersModule } from './modules/owners/owners.module';
-import { PrismaModule } from './modules/prisma/prisma.module';
 import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { HealthModule } from './modules/health/health.module';
+import { OwnersModule } from './modules/owners/owners.module';
 import { VenuesModule } from './modules/venues/venues.module';
+import { LocationsModule } from './modules/locations/locations.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     PrismaModule,
-    HealthModule,
     AuthModule,
     UsersModule,
+    HealthModule,
     OwnersModule,
     VenuesModule,
-    LocationsModule
-  ]
+    LocationsModule,
+  ],
 })
 export class AppModule {}
